975d17471be19f7c3921d730c4861664682c458d
김지수B <CoffeeAndJava@BrianKs-MacBook-Pro.local>
2017-04-10 19:07:09 +0900

    [test] learning MockMvc

--- src/test/java/market/controller/ControllerTest.java
+++ src/test/java/market/controller/ControllerTest.java
@@ -17,6 +17,12 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
 
 /**
  * Created by coffeenjava on 2017. 4. 2..
@@ -36,22 +42,36 @@ public class ControllerTest {
     @Before
     public void before() {
         mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        /**
+         * stand alone
+         * without @ setting
+         */
+//        mockMvc = MockMvcBuilders.standaloneSetup(new HomeController()).build();
     }
 
     @Test
     public void test() throws Exception {
-        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/"))
+        ResultActions resultActions = mockMvc.perform(get("/"));
+
+        MvcResult result = resultActions.andDo(print())
+                .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.view().name("home"))
                 .andReturn();
-        String content = result.getResponse().getContentAsString();
-        System.out.println("["+content+"]");
+
+//        String content = result.getResponse().getContentAsString();
     }
 
     @Test
     public void getMapTest() throws Exception {
-        ResultActions resultActions = mockMvc.perform(MockMvcRequestBuilders.get("/getMap"));
-        MvcResult result = resultActions.andReturn();
-        String content = result.getResponse().getContentAsString();
-        System.out.println(content);
+        ResultActions resultActions = mockMvc.perform(get("/getMap"));
+
+        MvcResult result = resultActions.andDo(print())
+                .andExpect(status().isOk())
+//                .andExpect(model().attributeExists("title"))
+                .andExpect(jsonPath("$.title").value("HelloTitle"))
+                .andReturn();
+//        MvcResult result = resultActions.andExpect(model().attributeExists("title"));
+//        String content = result.getResponse().getContentAsString();
+//        System.out.println(content);
     }
 }
